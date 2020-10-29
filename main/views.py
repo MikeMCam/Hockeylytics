ac80f58edfa20a621a34103973c36ef76b329e57
Michael Cam <mcam@uncc.edu>
2020-10-29 01:00:21 -0400

    Separated the html responses into template files

--- main/views.py
+++ main/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'MikeMDC',
+        'title': 'new post!',
+        'content': 'ha sike',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'MikeMDC1',
+        'title': 'secnd post!',
+        'content': 'ha ddddd',
+        'date_posted': 'August 27, 2019'
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>This is the home page</h1>')
-
+    context = {
+        'posts': posts
+    }
+    return render(request, 'main/home.html', context)
 
 def news(request):
-    return HttpResponse('<h1>This is the news page</h1>')
+    return render(request, 'main/news.html', {'title': 'News'})
 
 
 def about(request):
-    return HttpResponse('<h1>This is the about page</h1>')
+    return render(request, 'main/about.html', {'title': 'About'})
 
 
 def my_stats(request):
-    return HttpResponse('<h1>This is the my_stats page</h1>')
+    return render(request, 'main/my_stats.html', {'title': 'Stats'})
 
 
 def clients(request):
-    return HttpResponse('<h1>This is the clients page</h1>')
+    return render(request, 'main/clients.html', {'title': 'Clients'})
 
