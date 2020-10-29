e2594ccf39bd4de8427e987f820708d4262d8ada
Michael Cam <mcam@uncc.edu>
2020-10-29 01:52:11 -0400

    Added styling to the site, added links, logo, bootstrap, backbone for the website

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/', views.home, name='home', ),
     path('news/', views.news, name='news'),
     path('about/', views.about, name='about'),
     path('stats/', views.my_stats, name='my_stats'),
