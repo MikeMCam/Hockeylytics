2424fe822825bff999b0c5f4b5bd0e260534ede5
Michael Cam <mcam@uncc.edu>
2020-11-18 21:09:39 -0500

    You can now create, delete, and and update posts in the feedback forum.

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView, PostUpdateView, PostDeleteView)
 from . import views
 
 urlpatterns = [
@@ -7,5 +10,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('stats/', views.my_stats, name='my_stats'),
     path('clients/', views.clients, name='clients'),
-    path('feedback/', views.feedback, name='feedback'),
+    path('feedback/', PostListView.as_view(), name='feedback'),
+    path('feedback/<int:pk>/', PostDetailView.as_view(), name='feedback-detail'),
+    path('feedback/new/', PostCreateView.as_view(), name='feedback-create'),
+    path('feedback/<int:pk>/update/', PostUpdateView.as_view(), name='feedback-update'),
+    path('feedback/<int:pk>/delete/', PostDeleteView.as_view(), name='feedback-delete'),
+    # path('feedback/', views.feedback, name='feedback'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView)
 from .models import Post
-
 # from django.http import HttpResponse
 
 
-posts = [
-    {
-        'author': 'MikeMDC',
-        'title': 'new post!',
-        'content': 'ha sike',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'MikeMDC1',
-        'title': 'second post!',
-        'content': 'ha ddddd',
-        'date_posted': 'August 27, 2019'
-    },
-]
-
-
 def home(request):
-
     return render(request, 'main/home.html', {'title': 'Home'})
 
 
@@ -41,9 +26,58 @@ def clients(request):
     return render(request, 'main/clients.html', {'title': 'Clients'})
 
 
+"""""""""
 def feedback(request):
     context = {
         'posts': Post.objects.all(),
         'title': 'Feedback',
     }
     return render(request, 'main/feedback.html', context)
+"""""""""
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'main/feedback.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # Prevents non-author from editing post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/feedback/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
