7d6600199f39356f39f3a4d8db023884e769a00a
Michael Cam <mcam@uncc.edu>
2020-11-25 21:58:41 -0500

    pagination and custom 404

--- hockeylytics/settings.py
+++ hockeylytics/settings.py
@@ -24,9 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+zxfadf=r)m6t=vghd2a#=%6h&2nv#+-%(vxcf0*(!(i!lu3yy'
 
 ALLOWED_HOSTS = ['https://hockeylytics.herokuapp.com/',
-                 'https://hockeylytics.net/',
-                 'http://hockeylytics.net/',
-                 'www.hockeylytics.net/']
+                 '.hockeylytics.net']
 
 
 # Application definition
